7cab6dba6b9008e557222dab16d83df18c91815d
Laurent Parmentier <laubosslink@society-lbl.com>
2015-06-29 11:55:55 -0400

    master: enhancement test on php/php-inheritance

--- php/php-inheritance/test.php
+++ php/php-inheritance/test.php
@@ -9,6 +9,7 @@ class Test {
 class Test2 extends Test {
 	public function hello2() {
 		$this->hello(); // Check if $this mean also inheritance methods
+		parent::hello(); // Check if it work in spite of it is not static method
 	}
 }
 
